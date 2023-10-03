1e29e5969ef3bf60a5f67281f0b838b87707bf74
leedongha <73922876+GAP-dev@users.noreply.github.com>
2023-10-03 15:19:49 +0900

    이동하 1차 작업 커밋

---
+++ gachon_calculator.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+
+int add(int a, int b) {
+    int calc;
+
+
+    return calc;
+}
+
+int subtract(int a, int b) {
+    int calc;
+
+
+    return calc;
+}
+
+int multiply(int a, int b) {
+    int calc;
+
+
+    return calc;
+}
+
+int divide(int a, int b) {
+    int calc;
+
+
+    return calc;
+}
+
+int remainder(int a, int b) {
+    int calc;
+
+
+    return calc;
+}
+
+
+int main(void)
+{
+	int x;
+	int y;
+	char op;
+    int result;
+
+	printf("계산할 식을 입력하세요 >> ");
+	scanf("%d %c %d", &x, &op, &y);
+
+	return 0;
+}
+
+
